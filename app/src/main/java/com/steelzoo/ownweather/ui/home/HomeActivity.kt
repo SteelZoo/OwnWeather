c87bb2cb6646516ec52e42716eb396d7eb7ec147
SteelZoo <cjfwn96@gmail.com>
2023-02-14 19:00:58 +0900

    create: HomeViewModel

--- app/src/main/java/com/steelzoo/ownweather/ui/home/HomeActivity.kt
+++ app/src/main/java/com/steelzoo/ownweather/ui/home/HomeActivity.kt
@@ -1,10 +1,16 @@
 package com.steelzoo.ownweather.ui.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import com.steelzoo.ownweather.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
+
+    val homeViewmodel: HomeViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
