5880f283d404f3b0dc700170dbce015b1fbbebdd
SteelZoo <cjfwn96@gmail.com>
2023-02-17 00:49:04 +0900

    create: API Url 저장 파일 생성 (날씨api url 전역변수화)

---
+++ app/src/main/java/com/steelzoo/ownweather/data/UrlString.kt
@@ -0,0 +1,7 @@
+package com.steelzoo.ownweather.data
+
+const val GO_BASE_URL = "http://apis.data.go.kr"
+
+const val WEATHER_URL = "/1360000/VilageFcstInfoService_2.0"
+
+const val WEATHER_NOW_URL = "/getUltraSrtNcst"
