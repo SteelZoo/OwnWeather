47856c3cad05b4488063cc68a821b57f0aad10ee
SteelZoo <cjfwn96@gmail.com>
2023-02-28 22:08:19 +0900

    feat: 초단기 예보 url 추가

--- app/src/main/java/com/steelzoo/ownweather/data/UrlString.kt
+++ app/src/main/java/com/steelzoo/ownweather/data/UrlString.kt
@@ -5,3 +5,5 @@ const val GO_BASE_URL = "http://apis.data.go.kr"
 const val WEATHER_URL = "/1360000/VilageFcstInfoService_2.0"
 
 const val WEATHER_NOW_URL = "/getUltraSrtNcst"
+
+const val WEATHER_VERTSHORT_URL = "/getUltraSrtFcst"
