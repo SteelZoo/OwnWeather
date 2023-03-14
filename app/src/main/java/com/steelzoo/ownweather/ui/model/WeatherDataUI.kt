2502a16cc05ecc1c8a9604ebd18884d37538dd24
SteelZoo <cjfwn96@gmail.com>
2023-03-15 03:22:05 +0900

    fix: WeatherDataUI/ SkyState 위치 변경에 따른 import 추가

--- app/src/main/java/com/steelzoo/ownweather/ui/model/WeatherDataUI.kt
+++ app/src/main/java/com/steelzoo/ownweather/ui/model/WeatherDataUI.kt
@@ -1,5 +1,7 @@
 package com.steelzoo.ownweather.ui.model
 
+import com.steelzoo.ownweather.domain.model.SkyState
+
 data class WeatherDataUI(
     val skyState: SkyState,
     val temperature: Double,
