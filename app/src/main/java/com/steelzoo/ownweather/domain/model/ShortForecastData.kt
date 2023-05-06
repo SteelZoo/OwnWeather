62f06819d9b74aefbd1c94e7b7d1be6cce8a6901
SteelZoo <cjfwn96@gmail.com>
2023-05-07 03:07:57 +0900

    refactor: 기상청 원본 데이터 대응 일부 변수 자료형 변환
    - 강수량(RN1, PCP)에 "강수없음" 이라는 데이터 반환 됨
    - 숫자 + "mm" 형식으로 데이터 반환 됨

--- app/src/main/java/com/steelzoo/ownweather/domain/model/ShortForecastData.kt
+++ app/src/main/java/com/steelzoo/ownweather/domain/model/ShortForecastData.kt
@@ -1,10 +1,11 @@
 package com.steelzoo.ownweather.domain.model
 
 data class ShortForecastData (
+    val date: String,
     val time: String,
     val skyState: SkyState,
     val temperature: Int,
     val humidity: Int,
-    val precipitation: Double,
+    val precipitation: String,
     val probability: Int,
 )
